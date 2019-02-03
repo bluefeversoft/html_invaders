5f46a6423c8687f9bc0fd912887c740b735e8bb1
Bluefever <bluefeversoft@gmail.com>
2019-02-03 16:12:36 +0100

    filmed ch5

---
+++ ch5/js/defs.js
@@ -0,0 +1,51 @@
+const ImageFiles = [
+    'playerShip1_blue'
+];
+
+const GameSettings = {
+    keyPress: {
+        left: 37,
+        right: 39,
+        up: 38,
+        down: 40,
+        space: 32
+    },
+    playAreaWidth: 720,
+    playAreaHeight: 576,
+    playAreaDiv: '#playArea',
+
+    playerDivName: 'playerSprite',
+    playerStart: {
+        x: 360,
+        y: 440
+    }
+};
+
+let GameManager = {
+    assets : {},
+    player: undefined
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
